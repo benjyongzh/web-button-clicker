628fda0328f40d0905db5799a62f7a34ac741cc6
Benjamin Yong <benj_yongzh@hotmail.com>
2025-07-01 15:49:10 +0800

    Add --idcolumn. Fix target folder being absolute instead of relative

--- downloader.py
+++ downloader.py
@@ -7,6 +7,7 @@ import argparse
 import glob
 import os
 import shutil
+import time
 from utils import get_current_file_count,wait_for_download,format_filename,add_id_rows,get_latest_file
 
 # set args
@@ -15,8 +16,13 @@ parser.add_argument("--url", required=True, type=str,help="Enter URL of website 
 parser.add_argument("--buttonselector", required=True, type=str, help="Enter CSS selector of HTML element of button to click")
 parser.add_argument("--target", default="Downloads/", type=str, help="Enter relative file location to download file to")
 parser.add_argument("--filename", default="downloadfile", type=str, help="Enter filename to save as")
+parser.add_argument("--idcolumn", default=False, type=bool, help="Boolean if you want to add an ID column to the .csv file downloaded")
 parser.add_argument("--timeout", default=20, type=int, help="Enter timeout for downloading file")
 args = parser.parse_args()
+
+# ensure target folder exists
+target_abs = os.path.abspath(args.target)
+os.makedirs(target_abs, exist_ok=True)
 
 #webdriver options
 options = webdriver.ChromeOptions()
@@ -26,12 +32,13 @@ options.add_argument("--disable-notifications")
 # options.add_experimental_option('excludeSwitches', ['enable-logging'])
 prefs = {
         "download":{
-                "default_directory":args.target, ### Set the path accordingly
+                "default_directory":target_abs, ### Set the path accordingly
                 "directory_upgrade": True,
                 "prompt_for_download": False, ## change the downpath accordingly
                 "extensions_to_open": ""
                 },
-        "savefile.default_directory": args.target,
+        "savefile.default_directory": target_abs,
+        "safebrowsing.enabled": True,
         "profile.default_content_settings.popups": 0
         }
 options.add_experimental_option("prefs", prefs)
@@ -42,10 +49,10 @@ driver.get(args.url)
 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, args.buttonselector))).click()
 
 # wait until download is done
-file_count = get_current_file_count(args.target)
-wait_for_download(args.timeout, args.target, file_count)
+file_count = get_current_file_count(target_abs)
+wait_for_download(args.timeout, target_abs, file_count)
 driver.quit()
-print("file downloaded to: ", args.target)
+print("file downloaded to: ", target_abs)
 
 # print(get_latest_file(args.target))
 
@@ -53,13 +60,16 @@ print("file downloaded to: ", args.target)
 filename = format_filename(args.filename)
 
 # change filename
-files_path = os.path.join(args.target, '*')
+files_path = os.path.join(target_abs, '*')
 # print(files_path)
 list_of_files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True)
-# print("number of files:", len(list_of_files))
+print("number of files:", len(list_of_files))
+if not list_of_files:
+        raise FileNotFoundError(f"No downloaded files found in: {target_abs}")
 latest_file = list_of_files[0]
 oldext = os.path.splitext(latest_file)[1]
-shutil.move(latest_file,os.path.join(args.target,filename + oldext))
+shutil.move(latest_file,os.path.join(target_abs,filename + oldext))
 
-# add id columns
-add_id_rows(args.target + "/" + filename, "_with-id")
+if args.idcolumn:
+        # add id columns
+        add_id_rows(target_abs + "/" + filename, "_with-id")
